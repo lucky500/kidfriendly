a79a745e2e49c19cf155b3a8e44e88c32c4a0a87
lucky500 <luciano101@msn.com>
2016-10-15 11:45:43 -0400

    fixed pickadate picker

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
-//===== MAPBOX =======//	
+
+	//===== MAPBOX =======//	
 	L.mapbox.accessToken = 'pk.eyJ1IjoibHVja3k1MDAiLCJhIjoiY2l1MnZqcXA0MGh6ZjJ0cncxdnB5NzlpbiJ9.lbwU3q5yzU0W5XcI1NCV1g';
 	//initialize the map
 	var mapSimple = L.mapbox.map('map_simple', 'mapbox.streets')
  		 .setView([42.262593, -71.802293], 12); //lat, long
  	// disable map zoom when using scroll
-	mapSimple.scrollZoom.disable();	 
+	mapSimple.scrollWheelZoom.disable();	 
+
+	//============ MATERIALIZE MODS =============//
 
 
 
